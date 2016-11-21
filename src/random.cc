cef1a2f4e4381cd54a266c147a371a54cb0619a6
Victor Drobny <vdrobny@yandex.ru>
2016-11-21 22:25:33 +0100

    initial

---
+++ src/random.cc
@@ -0,0 +1,64 @@
+#include "random.h"
+
+#include <cmath>
+#include <random>
+
+namespace bdm {
+
+void Random::setSeed(long seed) {
+  seed_ = (seed ^ 25214903917L) & 281474976710655L;
+  next_next_gaussian_ = 0.0;
+  have_next_next_gaussian_ = false;
+}
+
+int Random::nextInt() { return std::rand(); }
+
+double Random::nextDouble() {
+  return (double)(((long)next(26) << 27) + (long)next(27)) *
+         1.1102230246251565E-16;
+}
+
+double Random::nextGaussian(double mean, double standard_deviation) {
+  return mean + standard_deviation * nextGaussian();
+}
+
+int Random::next(int var1) {
+  long var6 = seed_;
+
+  long var2;
+  long var4;
+  do {
+    var2 = var6;
+    var4 = (var2 * 25214903917L + 11L) & 281474976710655L;
+  } while (!compareAndSet(var6, var2, var4));
+  seed_ = var6;
+  return (int)(var4 >> (48 - var1));
+}
+
+bool Random::compareAndSet(long& current, long expected, long update) {
+  if (current == expected) {
+    current = update;
+    return true;
+  }
+  return false;
+}
+
+double Random::nextGaussian() {
+  if (have_next_next_gaussian_) {
+    have_next_next_gaussian_ = false;
+    return next_next_gaussian_;
+  } else {
+    double v1, v2, s;
+    do {
+      v1 = 2 * nextDouble() - 1;  // between -1.0 and 1.0
+      v2 = 2 * nextDouble() - 1;  // between -1.0 and 1.0
+      s = v1 * v1 + v2 * v2;
+    } while (s >= 1 || s == 0);
+    double multiplier = std::sqrt(-2 * std::log(s) / s);
+    next_next_gaussian_ = v2 * multiplier;
+    have_next_next_gaussian_ = true;
+    return v1 * multiplier;
+  }
+}
+
+}  // namespace bdm
